929be5aa6ec6b70677ca0c5fe80b4a21ad89fd2b
mutahiwachira <mutahi.wachira@gmail.com>
2022-06-25 19:21:14 +0200

    Species lookup functions not necessary. Removed.
    In the UI, we will have a selector which will populate with values for species names. Which means we can make it such that the user never selects something which is not in the table and, when they have made their selection, we immediately return a scientific name. Better than constantly checking it at the lowest level.

--- functions/data_access.R
+++ functions/data_access.R
@@ -7,15 +7,50 @@ load_polish_sample <- function(){
   return(df)
 }
 
-lookup_common_species_name <- function(lookup_table, common_name, species_name){
-  
-}
+# get_lookup_table <- function(){
+#   # this table has two columns - scientific_name and common_name
+#   # we will use it for matching common_names to scientific
+#   # we use scientific names everywhere because they are unique and regular
+#   lookup_table <- readr::read_csv("data/species_names_lookup.csv")
+#   lookup_table
+# }
+
+# lookup_species <- function(lookup_table = get_lookup_table(), species){
+#   # if not supplied, then we read the lookup table from our data
+#   # but the function allows you to supply it so that we minimize our dependence on read/write operations
+#   # the lookup table won't be too big and is something we might like to keep in memory because it is used always.
+#   
+#   # TODO: validate lookup table: can't have the same common name refer to two different scientific names
+#   
+#   # Use a common name to get a scientific_name
+#   all_scientific_names <- lookup_table$scientific_name
+#   
+#   species_scientific <- species[species %in% all_scientific_names]
+#   species_common     <- species[!species %in% all_scientific_names]
+#   
+#   # check if the species other is in the lookup table
+#   
+#   common_name_val <- common_name # otherwise filter doesn't work right - treats it as column == column
+#   scientific_names <- lookup_table |> 
+#     filter(common_name == common_name_val) |> 
+#     pull(scientific_name)
+#   
+#   # TODO: Defensive programming - the vectors should be the same length!
+#   
+#   # TODO: Defensive programming - what if they enter a nonsense value for the name?
+#   
+#   return(scientific_names)
+# }
 
 
 ## Query Functions ----
-qry_species <- function(data, scientific_name = NULL, common_name = NULL){
+qry_species <- function(data, species){
+  # query only works with scientific name at low level design
+  # higher level functions, UI, will always pass scientific names
   
-  # Use a common name to get a scientific_name
+  if(length(scientific_name) > 0){
+    # use scientific names if supplied
+  }
   
   df <- data |> 
     filter(scientific_name %in% scientific_name)
