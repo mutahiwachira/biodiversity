85c4339ab5263cd01cafe1503ebcc15deb896881
mutahiwachira <mutahi.wachira@gmail.com>
2022-06-25 19:14:49 +0200

    Writing functions to query the data in a safe and sensible way.
    Doing species lookup functions.

--- functions/data_access.R
+++ functions/data_access.R
@@ -3,14 +3,22 @@
 ## CSV Access / Database Access functions ----
 load_polish_sample <- function(){
   # Loads the polish csv data, a smaller dataframe for testing purposes
-  df <- readr::read_csv("data/poland_occurence.csv")
+  df <- readr::read_csv("data/biodiversity_poland.csv")
   return(df)
 }
 
+lookup_common_species_name <- function(lookup_table, common_name, species_name){
+  
+}
+
+
 ## Query Functions ----
-qry_species <- function(data, species_name){
+qry_species <- function(data, scientific_name = NULL, common_name = NULL){
+  
+  # Use a common name to get a scientific_name
+  
   df <- data |> 
-    filter(scientificName %in% species_name | vernacularName %in% species_name)
+    filter(scientific_name %in% scientific_name)
   
   return(df)
 }
@@ -20,10 +28,20 @@ qry_time_frame <- function(data, start = min(data$eventDate), end = max(data$eve
   end   <- as_date(end)
   
   df <- data |> 
-    filter(eventDate |> between(start, end))
+    filter(date |> between(start, end))
   
   return(df)
 }
 
+# This function gets the basic counts for a given time range and species to map
+# It's important to have this function because it aggregates over the time variable
+# It makes sure we don't overplot multiple observations in the same place from different times
+get_data_to_map <- function(data, start = min(data$eventDate), end = max(data$eventDate), scientific_name = NULL, common_name = NULL){
+  data |> 
+    qry_species(scientific_name, common_name) |> 
+    qry_time_frame(start, end) |> 
+    group_by(-date)
+}
 
 # Tests to implement
+
